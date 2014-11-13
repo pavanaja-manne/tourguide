7f9c436cc7922ad95ae9b7e52139f098ef9352e1
raghavendra-prithvi <raghava.sangars@gmail.com>
2014-11-13 21:00:32 +0000

    oauth

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -3,5 +3,6 @@ Rails.application.config.middleware.use OmniAuth::Builder do
   #provider :twitter, 'CONSUMER_KEY', 'CONSUMER_SECRET'  
   provider :facebook, '371633809663004', '88ee21c79668407935e67b4205393568'
   #provider :linked_in, 'CONSUMER_KEY', 'CONSUMER_SECRET'  
+ 
 end  
 

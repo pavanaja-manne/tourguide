9c16cf6b6b4c7fad43e87f38667316a591210aa3
raghavendra-prithvi <raghava.sangars@gmail.com>
2014-11-13 21:15:53 +0000

    facebook

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,8 +1,8 @@
 Rails.application.config.middleware.use OmniAuth::Builder do  
   OpenSSL::SSL::VERIFY_PEER = OpenSSL::SSL::VERIFY_NONE
   #provider :twitter, 'CONSUMER_KEY', 'CONSUMER_SECRET'  
-  provider :facebook, '371633809663004', '88ee21c79668407935e67b4205393568'
+  provider :facebook, '206096812769730', '94e70ea52ccae94416aa6baf8d37808c'
+  #provider :facebook, '371633809663004', '88ee21c79668407935e67b4205393568'
   #provider :linked_in, 'CONSUMER_KEY', 'CONSUMER_SECRET'  
- 
 end  
 

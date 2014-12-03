2b66b93b578540ee0a0f21ef79a59af634b4db9d
raghavendra-prithvi <raghava.sangars@gmail.com>
2014-12-03 00:14:47 +0000

    data

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -36,8 +36,8 @@ TourGuide::Application.configure do
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-    address: "smtp.gmail.com",git add 
-    port: 587,
+    address: "smtp.gmail.com",     
+    port: 27,
     domain: "gmail.com",
     authentication: "plain",
     enable_starttls_auto: true,
